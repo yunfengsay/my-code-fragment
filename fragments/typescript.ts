bc2fc5536fb14b33979992ce2b0b4244d95872b7
yunfengzhang.zyf <yunfengzhang.zyf@alibaba-inc.com>
2021-06-04 13:10:01 +0800

    fix

---
+++ fragments/typescript.ts
@@ -0,0 +1,21 @@
+export const safeParse = (data: any, defaultValue?: any) => {
+  if (typeof data === 'object') {
+    return data;
+  }
+  try {
+    return JSON.parse(data);
+  } catch (e) {
+    console.error(data);
+    console.error(e);
+    return defaultValue;
+  }
+};
+
+export const tryCatchFn = (data: any, fn: Function) => {try {return fn(data)} catch(e){return null}};
+
+export const safeStringify = (target: any) => (typeof target === 'object')? tryCatchFn(target, JSON.stringify): target;
+
+export const jf = (data: any, space = 4) => {
+  return JSON.stringify(data, null, space);
+};
+
