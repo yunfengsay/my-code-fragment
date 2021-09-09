09420fab8f751b6ecd4efa88cc98baa03d4f3313
yunfengzhang.zyf <yunfengzhang.zyf@alibaba-inc.com>
2021-09-09 19:51:46 +0800

    feat:hooks_sample

--- fragments/shell.sh
+++ fragments/shell.sh
@@ -3,4 +3,9 @@ xcrun simctl list | grep Booted | awk -F '[()]+' '{print $2}'
 # 添加媒体问价到ios模拟器
 xcrun simctl addmedia $(设备id) $(文件路径)
 
+# mac 截图, -c 保存在粘贴板中
+screencapture -i /tmp/test.png
+
+# 截取屏幕上的图片并发送给模拟器
+screencapture -i /tmp/screencapture.png && xcrun simctl addmedia $(xcrun simctl list | grep Booted | awk -F '[()]+' '{print $2}') /tmp/screencapture.png
 
